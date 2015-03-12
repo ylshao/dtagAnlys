654ca84d429316b7610430a59494299ed04bb4e5
ylshao <ylshao@umich.edu>
2015-03-11 21:10:02 -0400

    read data done

--- estOrient.m
+++ estOrient.m
@@ -1,4 +1,4 @@
-function TagData = estOrient(TagData, nTagData, OrientTab)
+function TagData = estOrient(TagData, nTagData, orientTab)
 
 %% If want to continue to save all the data with p,r,h estimation
 % accelTag = TagData(nTagData).accelTag;
@@ -6,16 +6,16 @@ recdir = 'M:\DQ_All_Day_Data';
 prefix = TagData(nTagData).deployName;
 df = TagData(nTagData).desampFreq;
 % % sampleFreq = TagData(nTagData).sampleFreq;
-isDeg = find(OrientTab(:, 3:5) > pi/2);
+isDeg = find(orientTab(:, 3:5) > pi/2);
 if isDeg
-TagData(nTagData).OrientTabDeg = OrientTab;    
-OrientTab = deg2rad(OrientTab);
+TagData(nTagData).orientTabDeg = orientTab;    
+orientTab = deg2rad(orientTab);
     else
-TagData(nTagData).OrientTabRad = OrientTab;
+TagData(nTagData).orientTabRad = orientTab;
 end
 % [accelAnim, magAnim] = tag2whale(accelTag,magTag,OrientTab,sampleFreq);
 % All_Plot(p, Aw, Mw, fs)
-d3savecal(prefix,'OTAB',OrientTab);
+d3savecal(prefix,'OTAB',orientTab);
 % d3makeprhfile(recdir,prefix,deploy_name,df)
 TagData = d3makeprhfile_simple(prefix,df, TagData, nTagData);
 % TagData = d3makeprhfile_mod(recdir,prefix,prefix,df, TagData, nTagData);
---
+++ shiftDepth.m
@@ -0,0 +1,47 @@
+function TagData = shiftDepth(TagData, nTagData, method)
+
+accelTag = TagData(nTagData).accelTag;
+depth = TagData(nTagData).depth;
+timeHour = TagData(nTagData).timeHour;
+
+depth(isnan(accelTag)) = nan;
+depth = -depth;
+% DataPlot_Feb1(t_h, p_trc, 'dep');
+THLD_RUB = 0.05;
+[timeHourPeak, depthPeak, indPeak] = getPeaks(timeHour, depth, THLD_RUB, 0, 0);
+
+
+THLD_LARGE_DESC = -0.02;
+THLD_LARGE_ASC = 0.02;
+
+if strcmpi(method, 'interp')
+    for i = 1:3
+    largeDesc = diff([0; depthPeak]) < THLD_LARGE_DESC;
+    largeAsc = diff([depthPeak; 0]) > THLD_LARGE_ASC;
+    isPeak = ~(largeDesc | largeAsc);
+    depthPeak = depthPeak(isPeak);
+    indPeak = indPeak(isPeak);
+    timeHourPeak = timeHourPeak(isPeak);
+    end
+    shiftDepthMeter = interp1(timeHourPeak, depthPeak, timeHour, 'pchip','extrap');
+  
+elseif strcmpi(method, 'max')
+    shiftDepthMeter = nan(numel(depth), 1);
+    dep = [depthPeak(1); depthPeak; depthPeak(end)];
+    indPeak = [1; indPeak; length(t_h)];
+    ARVG_WIN = 10;
+    shiftDepthMeter(1:indPeak(1+floor(ARVG_WIN/2))) = max(dep(1:1+floor(ARVG_WIN/2)));
+    for i = 1+floor(ARVG_WIN/2):length(indPeak)-floor(ARVG_WIN/2)
+        shiftDepthMeter(indPeak(i):indPeak(i+1)) = max(dep(i-floor(ARVG_WIN/2):i+floor(ARVG_WIN/2)));
+    end
+    shiftDepthMeter(indPeak(i+1):end) = max(dep(length(indPeak)-floor(ARVG_WIN/2)+1:end));
+end
+
+figure; plot(timeHour, depth); hold on; plot(timeHourPeak, depthPeak, 'r.')
+plot(timeHour, shiftDepthMeter, 'c.')% figure; plot(t_h, p_trc); hold on; plot(t_h, depthShift, 'c.--')
+
+
+depthShift = depth-shiftDepthMeter;
+TagData(nTagData).depthShift = depthShift;
+
+end
