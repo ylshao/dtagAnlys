d4534cf8fe4ce9114604ac03668c842e75723262
ylshao <ylshao@umich.edu>
2015-03-14 23:18:11 -0400

    March 14 done

--- initPath.m
+++
@@ -1,7 +0,0 @@
-function initPath(recdir)
-
-settagpath('cal',[recdir, '\cal']);
-%settagpath('raw','c:/tag/data/raw');
-settagpath('prh',[recdir, '\prh']);
-settagpath('audit',[recdir, '\audit']) ;
-end
